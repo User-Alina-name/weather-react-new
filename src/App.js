f24085a4eab2c4c5a84fa06a4ba8f810e68ffa24
User-Alina-name <pismenna.alina18@gmail.com>
2022-10-04 19:19:13 +0200

    add live data +bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ function App() {
   return (
     <div className="App">
       <Weather defaultCity="Zurich" />
+      <footer className="source">
+        Coded by Alina Pysmenna, open-source code on
+        <a
+          href="https://github.com/User-Alina-name/weather-project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          _GitHub_
+        </a>
+        and hosted on
+        <a
+          href="https://react-weather-by-pysmenna-alina.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          _Netlify_
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,80 +1,67 @@
 import React, { useState } from "react";
 import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
+    console.log(response);
     setWeatherData({
       ready: true,
       temperature: response.data.main.temp,
-      date: "Wednesday 07:00",
+      date: new Date(response.data.dt * 1000),
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      icon: response.data.weather[0].icon,
       city: response.data.name,
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "472212a9a6af5b33f2a57994d7532dbe";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (weatherData.ready) {
     return (
-      <div className="Weather">
-        <div className="container">
-          <div className="content">
-            <h1 className="main-text" id="cityName">
-              {weatherData.city}{" "}
-            </h1>
-            <form className="search-city">
+      <div className="container">
+        <form className="search-city" onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
               <input
                 className="search-width"
-                type="text"
+                type="search"
                 id="search"
+                autoFocus="on"
                 placeholder="Search city..."
+                onChange={handleCityChange}
               />
+            </div>
+            <div className="col-3">
               <input className="lupa" type="submit" value="🔎" />
-            </form>
-            <div id="square">
-              <span className="namedDay">{weatherData.data}</span>
-              <h4 className="today">
-                <img
-                  src={weatherData.iconUrl}
-                  alt={weatherData.description}
-                  className="icon"
-                ></img>
-                <strong className="deg degrees"></strong>
-                <span className="units"></span>
-                <button href="#" id="celcius">
-                  {Math.round(weatherData.temperature)} °C
-                </button>
-                |<button id="fahrenheit">°F</button>
-                <div className="unit cloudy"></div>
-                wind:
-                <span className="unit windSpeed">
-                  {" "}
-                  {Math.round(weatherData.wind)}
-                </span>
-                <br />
-                humidity:
-                <span className="unit humidity"> {weatherData.humidity}</span>
-              </h4>
             </div>
           </div>
-        </div>
-        <footer className="source">
-          <a href="https://github.com/User-Alina-name/weather-project">
-            Open-source code
-          </a>
-          (by Alina Pysmenna)
-        </footer>
+        </form>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
-    const apiKey = `6e6ec494746b5229a9f2d526478c924c`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
   }
 }
