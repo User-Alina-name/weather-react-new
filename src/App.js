6744525462e66bd8b19cd5159b38608090e0bf6c
User-Alina-name <pismenna.alina18@gmail.com>
2022-10-06 13:04:50 +0200

    mail add

--- src/App.js
+++ src/App.js
@@ -6,22 +6,32 @@ function App() {
     <div className="App">
       <Weather defaultCity="Zurich" />
       <footer className="source">
-        Coded by Alina Pysmenna, open-source code on
-        <a
-          href="https://github.com/User-Alina-name/weather-project"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          _GitHub_
-        </a>
-        and hosted on
-        <a
-          href="https://react-weather-by-pysmenna-alina.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          _Netlify_
-        </a>
+        <div>
+          Coded by{" "}
+          <a
+            href="mailto:pismenna.alina18@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Alina Pysmenna
+          </a>
+          , open-source code on{" "}
+          <a
+            href="https://github.com/User-Alina-name/weather-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://react-weather-by-pysmenna-alina.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </div>
       </footer>
     </div>
   );
